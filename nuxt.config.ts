284c3b77cd06e064d24dc20a5800ede9a8edb3b7
Seth Sharp <sesharp@outlook.com>
2024-10-26 11:58:30 +1000

    add pages to prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: false,
-      routes: ['/'],
+      routes: ['/', '/projects', '/experiences', '/contact'],
     },
   },
 
