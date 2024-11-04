2e818f8640c64eb777b72da98339e9b888ffa83d
Seth Sharp <sesharp@outlook.com>
2024-11-04 14:28:46 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: false,
-      routes: ['/', 'error', 'contact', 'experience', 'projects'],
+      routes: ['/', 'error', '/contact', '/experience', '/projects'],
     },
   },
 
