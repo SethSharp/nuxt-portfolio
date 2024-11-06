7abdfdd030912d9d65864eedf58671cddbcc3a8c
Seth Sharp <sesharp@outlook.com>
2024-11-06 13:19:57 +1000

    sheet for mobile dropdown

--- components/navigation/Bar.vue
+++ components/navigation/Bar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Bars3Icon } from "@heroicons/vue/24/outline"
+import {Sheet, SheetClose, SheetContent, SheetTrigger, LumuixModeToggle} from "@sethsharp/lumuix"
 import { type Link } from "~/types"
 
 defineProps<{
@@ -10,27 +11,24 @@ const open = ref(false)
 </script>
 
 <template>
-  <nav class="h-fit sm:py-4 z-10 w-full">
+  <nav class="h-fit sm:py-4 p-2">
     <div class="h-full gap-6 justify-center hidden sm:flex">
       <div class="my-auto space-x-4">
         <NavigationLink v-for="link in links" :link="link" />
       </div>
     </div>
 
-    <div class="sm:hidden">
-      <div class="mx-4 relative">
-        <div class="flex gap-6 py-3">
-          <button @click="open = ! open" class="hover:bg-gray-800 rounded transition cursor-pointer">
-            <Bars3Icon class="size-10 text-gray-500"/>
-          </button>
+    <Sheet>
+      <SheetTrigger class="sm:hidden" as-child>
+        <Bars3Icon class="size-10 text-gray-500 hover:bg-gray-800"/>
+      </SheetTrigger>
+
+      <SheetContent side="top">
+        <div
+            class="rounded w-full flex justify-start shadow-2xl p-3 space-y-2 flex-col">
+          <NavigationLink v-for="link in links" :link="link" />
         </div>
-        <div v-if="open" class="flex transition justify-start mt-4 z-50">
-          <div
-              class="bg-gray-700 rounded w-full sm:w-1/2 z-50 absolute text-left shadow-2xl px-6 py-6 space-y-2 grid">
-            <NavigationLink v-for="link in links" :link="link" />
-          </div>
-        </div>
-      </div>
-    </div>
+      </SheetContent>
+    </Sheet>
   </nav>
 </template>
